4b9006b62942bf8f55b786fe79cb6e68adc07f4b
Ken <ken@vanillacoding.co>
2024-06-28 10:33:17 +0900

    Temporary for js101

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,182 @@
 const CHAT_HISTORY = [
   {
     userId: 1,
-    username: "켄",
-    createdAt: "2023-02-14T18:59:55.628Z",
-    text: "안녕하세요!",
-    colorCode: "#A1B7E5",
-  },
-  {
-    userId: 2,
-    username: "슈가",
-    createdAt: "2023-02-14T19:00:04.000Z",
+    nickname: "RM",
+    createdAt: '2024-06-27T18:06:40.036Z',
     text: "안녕하세요. :)",
-    colorCode: "#6534F4",
-  },
-  {
-    userId: 3,
-    username: "푸우",
-    createdAt: "2023-02-14T19:03:01.000Z",
-    text: "안녕하세요..ㅎㅎ",
-    colorCode: "#386106",
-  },
-  {
-    userId: 1,
-    username: "켄",
-    createdAt: "2023-02-14T19:10:11.000Z",
-    text: "쪼꼬렛 드셨나요?",
-    colorCode: "#A1B7E5",
-  },
-  {
-    userId: 4,
-    username: "켄",
-    createdAt: "2023-02-14T19:14:02.000Z",
-    text: "안녕하십니까...",
-    colorCode: "#8F958E",
-  },
-  {
-    userId: 1,
-    username: "켄",
-    createdAt: "2023-02-14T19:16:08.000Z",
-    text: "헛..?!",
-    colorCode: "#A1B7E5",
   },
   {
     userId: 2,
-    username: "슈가",
-    createdAt: "2023-02-14T19:21:11.000Z",
-    text: "두 분이 닉넴이 같으시네요 ㅋㅋ",
-    colorCode: "#6534F4",
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
+  },
+  {
+    userId: 2,
+    nickname: "푸우",
+    createdAt: '2024-06-27T18:06:40.036Z',
+    text: "안녕하세요..ㅎㅎ",
   },
 ];
 
-/*
- * 랜덤 Hex code 생성 함수
- */
-function generateHexCode() {
-  const letters = "0123456789ABCDEF";
-  let result = "#";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.2/firebase-database.min.js";
 
-  for (let i = 0; i < 6; i++) {
-    result += letters[Math.floor(Math.random() * 16)];
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyDMT5s6JPJPRjnzL8FdWN3Ojpfz_gAjs44",
+  authDomain: "chit-chat-7ee93.firebaseapp.com",
+  databaseURL: "https://chit-chat-7ee93-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chit-chat-7ee93",
+  storageBucket: "chit-chat-7ee93.appspot.com",
+  messagingSenderId: "657104305224",
+  appId: "1:657104305224:web:1869e92c514eff31bdec7d",
+  measurementId: "G-Y718BEJV9F"
+};
 
-  return result;
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const chatRef = ref(db, '/');
+
+async function fb () {
+  await set(chatRef, {
+    something: 1
+  });
 }
 
-for (let i = 0; i < CHAT_HISTORY.length; i++) {
-  const message = CHAT_HISTORY[i];
+fb();
 
-  console.log(message);
+onValue(chatRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+});
+
+const signInPage = document.querySelector(".signin");
+const chatPage = document.querySelector(".chat");
+const overlay = document.querySelector(".overlay");
+
+const signInForm = document.querySelector(".signin form");
+const nicknameInput = document.querySelector(".signin input");
+
+const currentUser = {
+  userId: Math.floor(Math.random() * 1000000),
+  nickname: "",
+};
+
+signInForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (nicknameInput.value.trim() === "") {
+    alert("닉네임을 입력해주세요.");
+    return;
+  }
+
+  currentUser.nickname = nicknameInput.value;
+
+  signInPage.classList.add("hide");
+  chatPage.classList.remove("transparent");
+  overlay.classList.remove("transparent");
+
+  loadMessages();
+});
+
+const messageInputButton = document.querySelector(".message-input button");
+const messageInputText = document.querySelector(".message-input textarea");
+
+messageInputButton.addEventListener("click", () => {
+  if (messageInputText.value.trim() === "") {
+    alert("메시지를 입력해주세요.");
+    return;
+  }
+
+  const message = {
+    userId: currentUser.userId,
+    nickname: currentUser.nickname,
+    createdAt: new Date().toISOString(),
+    text: messageInputText.value,
+  };
+
+  const db = getDatabase();
+  set(ref(db, 'chit-chat'), message);
+
+  messageInputText.value = "";
+});
+
+function loadMessages () {
+  for (let i = 0; i < CHAT_HISTORY.length; i++) {
+    const message = CHAT_HISTORY[i];
+
+    addMessage(message);
+  }
+}
+
+function addMessage (message) {
+  const html = `<div class="message">
+    <p>${message.text}</p>
+    <p class="nickname">💁🏻‍♂️ ${message.nickname}</p>
+    <p>${formatDate(message.createdAt)}</p>
+  </div>`;
+
+  const newMessage = document.createElement("div");
+  newMessage.innerHTML = html;
+
+  if (message.userId === currentUser.userId) {
+    newMessage.classList.add("right");
+  }
+
+  const messages = document.querySelector(".messages");
+
+  messages.prepend(newMessage);
+}
+
+function formatDate (dateString) {
+  const d = new Date(dateString);
+
+  const month = d.getMonth() + 1;
+  const date = d.getDate();
+
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  const ampm = hours >= 12 ? "오후" : "오전";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  const time = `${ampm} ${hours}시 ${minutes}분`;
+
+  return `${month}월 ${date}일 ${time}`;
 }
