571ca41ad4f4d92aca049c97ca5833894c0da078
Ken <ken@vanillacoding.co>
2024-06-30 01:58:47 +0900

    Work ready

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,11 @@
+// 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
 export const firebaseConfig = {
-  apiKey: "AIzaSyDMT5s6JPJPRjnzL8FdWN3Ojpfz_gAjs44",
-  authDomain: "chit-chat-7ee93.firebaseapp.com",
-  databaseURL: "https://chit-chat-7ee93-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chit-chat-7ee93",
-  storageBucket: "chit-chat-7ee93.appspot.com",
-  messagingSenderId: "657104305224",
-  appId: "1:657104305224:web:1869e92c514eff31bdec7d",
-  measurementId: "G-Y718BEJV9F"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
